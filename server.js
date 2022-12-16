f9c7a6f9b7a6989714d4ecf0ff5d8982581660e7
Ohm <nuengwong.t@cp.eng.chula.ac.th>
2022-12-16 20:38:52 +0700

    Connect with MongoDB Atlas

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const hospitals = require('./routes/hospitals');
+
+dotenv.config({ path: './config/config.env' });
+
+connectDB();
+
+const app = express();
+//Body parser
+app.use(express.json());
+
+app.use('/api/v1/hospitals', hospitals);
+
+const PORT = process.env.PORT || 5000;
+const server = app.listen(
+  PORT,
+  console.log(
+    'Server running in ',
+    process.env.NODE_ENV,
+    ' mode on port ',
+    PORT
+  )
+);
+
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error:${err.message}`);
+  server.close(() => process.exit(1));
+});
