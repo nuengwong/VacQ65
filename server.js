48206b27d7ebba12e2b5616d5487f4a61819e21d
Ohm <nuengwong.t@cp.eng.chula.ac.th>
2022-12-17 16:29:01 +0700

    Authentication

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
 const connectDB = require('./config/db');
 const hospitals = require('./routes/hospitals');
+const auth = require('./routes/auth');
 
 dotenv.config({ path: './config/config.env' });
 
@@ -11,7 +13,11 @@ const app = express();
 //Body parser
 app.use(express.json());
 
+//Cookie parser
+app.use(cookieParser());
+
 app.use('/api/v1/hospitals', hospitals);
+app.use('/api/v1/auth', auth);
 
 const PORT = process.env.PORT || 5000;
 const server = app.listen(
